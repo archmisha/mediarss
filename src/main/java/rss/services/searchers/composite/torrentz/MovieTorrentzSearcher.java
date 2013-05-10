6f86393f8cd49b9a1e6b57f1fc49b4c295631f9b
Michael Dikman <michael.dikman@hp.com>
2013-05-10 17:48:14 +0300

    [movies] fix double torrents for some movies like maniac 2012

--- src/main/java/rss/services/searchers/composite/torrentz/MovieTorrentzSearcher.java
+++ src/main/java/rss/services/searchers/composite/torrentz/MovieTorrentzSearcher.java
@@ -2,14 +2,13 @@ package rss.services.searchers.composite.torrentz;
 
 import org.springframework.stereotype.Service;
 import rss.entities.Movie;
+import rss.entities.Torrent;
 import rss.services.requests.MovieRequest;
 import rss.services.searchers.SearchResult;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * User: dikmanm
@@ -30,17 +29,39 @@ public class MovieTorrentzSearcher extends TorrentzSearcher<MovieRequest, Movie>
 
 	@Override
 	protected SearchResult processTorrentzResults(MovieRequest originalRequest, Set<TorrentzResult> foundRequests) {
+		// group requests by name and for each name leave only the one with the best seeders
+		// this way we eliminate torrents with same name and lower seeders number
+		Map<String, TorrentzResult> map = new HashMap<>();
+		for (TorrentzResult foundRequest : foundRequests) {
+			String title = foundRequest.getTitle();
+			if (map.containsKey(title)) {
+				if (map.get(title).getUploaders() < foundRequest.getUploaders()) {
+					map.put(title, foundRequest);
+				}
+			} else {
+				map.put(title, foundRequest);
+			}
+		}
+
 		List<SearchResult> results = new ArrayList<>();
 		SearchResult awaitingAgingResult = null;
-		for (TorrentzResult foundRequest : foundRequests) {
+		for (TorrentzResult foundRequest : map.values()) {
 			MovieRequest curRequest = new MovieRequest(foundRequest.getTitle(), foundRequest.getHash());
 			curRequest.setUploaders(foundRequest.getUploaders());
 			enrichRequestWithSearcherIds(curRequest);
+
 			CompositeSearcherData compositeSearcherData = new CompositeSearcherData();
 			super.searchHelper(curRequest, compositeSearcherData);
 			SearchResult searchResult = compositeSearcherData.getSuccessfulSearchResult();
+
 			// null means not found
 			if (searchResult != null) {
+				// override torrent titles, because it might differ from the torrentz side, which combines all
+				// other sites. And the we get double torrent names
+				for (Torrent torrent : searchResult.getTorrents()) {
+					torrent.setTitle(foundRequest.getTitle());
+				}
+
 				if (searchResult.getSearchStatus() == SearchResult.SearchStatus.FOUND) {
 					results.add(searchResult);
 				} else if (searchResult.getSearchStatus() == SearchResult.SearchStatus.AWAITING_AGING) {
