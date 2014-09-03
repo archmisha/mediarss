d3c7ed10b509857768bdfd861a596a2adf9a38ef
Michael Dikman <michael.dikman@hp.com>
2014-09-03 21:01:46 +0300

    download shows list once a week instead of once a day

--- src/main/java/rss/services/shows/ShowsListDownloaderServiceImpl.java
+++ src/main/java/rss/services/shows/ShowsListDownloaderServiceImpl.java
@@ -3,21 +3,16 @@ package rss.services.shows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
-import org.springframework.transaction.support.TransactionTemplate;
 import rss.services.JobRunner;
 import rss.util.QuartzJob;
-
-import javax.annotation.PostConstruct;
 
 /**
  * User: dikmanm
  * Date: 31/12/12 21:00
  */
 @Service
-@QuartzJob(name = "ShowsListDownloader", cronExp = "0 0 3/24 * * ?")
+@QuartzJob(name = "ShowsListDownloader", cronExp = "0 0 0 1/7 * ?")
 public class ShowsListDownloaderServiceImpl extends JobRunner implements ShowsListDownloaderService {
 
 	@Autowired
