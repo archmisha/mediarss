0660f287b278205e90a63562e45380c62a883efc
Michael Dikman <michael.dikman@hpe.com>
2016-02-06 22:47:16 +0200

    download tomcat instead of storing in git

--- runtime/build/h2/build.sh
+++ runtime/build/h2/build.sh
@@ -1,6 +1,6 @@
 docker build --tag archmisha/mediarss_h2 .
 docker login -u archmisha -p Aa123456 -e archmisha@gmail.com https://index.docker.io/v1/
 docker push archmisha/mediarss_h2
-# docker run -p 27017:27017 --name mongo_instance_001 -d archmisha/mediarss_mongodb
-# docker exec -i -t archmisha/mediarss_mongodb /bin/bash
+# docker run -p 1521:1521 -p 81:81 -v /c/Misha/Dev/data/h2:/data/h2-data --name h2_instance_001 -d archmisha/mediarss_h2
+# docker exec -i -t <id> /bin/bash
 # mongo --port 27017
--- runtime/build/mongodb/build.sh
+++ runtime/build/mongodb/build.sh
@@ -2,5 +2,5 @@ docker build --tag archmisha/mediarss_mongodb .
 docker login -u archmisha -p Aa123456 -e archmisha@gmail.com https://index.docker.io/v1/
 docker push archmisha/mediarss_mongodb
 # docker run -p 27017:27017 --name mongo_instance_001 -d archmisha/mediarss_mongodb
-# docker exec -i -t archmisha/mediarss_mongodb /bin/bash
+# docker exec -i -t <id> /bin/bash
 # mongo --port 27017
