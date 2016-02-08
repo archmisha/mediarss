9fbdab063a274dd6fc2ccfe17557fbaeb06401f0
Michael Dikman <michael.dikman@hpe.com>
2016-02-08 17:39:16 +0200

    fix docker-start batch

--- runtime/build/docker-env.bat
+++ runtime/build/docker-env.bat
@@ -1,6 +1,6 @@
-SET DOCKER_TLS_VERIFY=1
-SET DOCKER_HOST=tcp://192.168.99.100:2376
-SET DOCKER_CERT_PATH=C:\Users\mishad\.docker\machine\machines\default
-SET DOCKER_MACHINE_NAME=default
+REM SET DOCKER_TLS_VERIFY=1
+REM SET DOCKER_HOST=tcp://192.168.99.100:2376
+REM SET DOCKER_CERT_PATH=C:\Users\mishad\.docker\machine\machines\default
+REM SET DOCKER_MACHINE_NAME=default
 REM Run this command to configure your shell: 
-REM 	FOR /f "tokens=*" %i IN ('"C:\Program Files\Docker Toolbox\docker-machine.exe" env -shell cmd default') DO %i
+FOR /f "tokens=*" %i IN ('"C:\Program Files\Docker Toolbox\docker-machine.exe" env -shell cmd default') DO %i
