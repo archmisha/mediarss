a7ad60da984e30fd0997a229b6395afc945cfa40
Michael Dikman <michaeld@perfectomobile.com>
2016-02-20 20:34:10 +0200

    spring 4, oog4j2, hibernate5

--- deployer/src/main/java/rss/deployer/Deployer.java
+++ deployer/src/main/java/rss/deployer/Deployer.java
@@ -1,8 +1,8 @@
 package rss.deployer;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
